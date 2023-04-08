156697a3d9d00c8001b5cb19bea5cc4e65a903e3
ugwuejeh <ugwudavidejeh@gmail.com>
2023-04-08 01:05:03 +0100

    Update boolean3.py

--- boolean3.py
+++ boolean3.py
@@ -1,9 +1,16 @@
+#Create a python function that calculate the age of student, if the student 
+# age is greater than or equal to 18 it should return true else false
+
+
 import datetime
 
-def student_age():
-    birth_year = int(input("enter yuor birth year: "))
-    current_year = datetime.datetime.now().year
 
+def student_age():
+    # student are allowed to input their names
+    birth_year = int(input("enter yuor birth year: "))
+    # calculation of curent year using datetime module
+    current_year = datetime.datetime.now().year
+  
     age = current_year - birth_year
     if age >= 18:
 
@@ -13,6 +20,3 @@ def student_age():
       
     
 student_age()
-
-
-
